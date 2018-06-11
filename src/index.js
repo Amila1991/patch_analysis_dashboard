f72c403a14158180e53ccef8fb938a8baf395a70
Amila Karunathilaka <amilakarunathilaka@gmail.com>
2018-06-11 15:22:34 +0530

    init react project

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import BaseComponent from "./component/baseComponent/baseComponent";
+// import registerServiceWorker from './registerServiceWorker';
+
+
+ReactDOM.render(
+    <BaseComponent/>
+    , document.getElementById('root'));
+// registerServiceWorker();
